139237421c1cc7a20b211f4b0a9052812af3b701
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-03-19 13:45:55 +0300

    Added eventuate local support. Added build scripts.

---
+++ start-site/src/main/java/io/spring/start/site/CustomInitializrConfiguration.java
@@ -0,0 +1,27 @@
+package io.spring.start.site;
+
+import io.spring.initializr.metadata.InitializrMetadataBuilder;
+import io.spring.initializr.metadata.InitializrProperties;
+import io.spring.initializr.web.support.DefaultInitializrMetadataProvider;
+import io.spring.initializr.web.support.InitializrMetadataUpdateStrategy;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(CustomInitializrProperties.class)
+public class CustomInitializrConfiguration {
+
+	@Bean
+	public DefaultInitializrMetadataProvider customInitializrMetadataProvider(InitializrProperties initializrProperties,
+																			  CustomInitializrProperties customInitializrProperties,
+																			  InitializrMetadataUpdateStrategy initializrMetadataUpdateStrategy) {
+		return new DefaultInitializrMetadataProvider(
+				InitializrMetadataBuilder
+						.fromInitializrProperties(customInitializrProperties.getInitializr())
+						.withInitializrProperties(initializrProperties, true)
+						.build(),
+				initializrMetadataUpdateStrategy);
+	}
+
+}
